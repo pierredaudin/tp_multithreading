92a752cd1e530effca31eb2046435e7d9072a524
Pierre Daudin <pierre.daudin@univ-tlse3.fr>
2024-12-05 14:29:25 +0100

    ajout conversion json

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 import time
-
+import json
 import numpy as np
 
 
@@ -19,3 +19,38 @@ class Task:
         start = time.perf_counter()
         self.x = np.linalg.solve(self.a, self.b)
         self.time = time.perf_counter() - start
+
+    def to_json(self):
+        task_dict = {
+            "identifier": self.identifier,
+            "size": self.size,
+            "a": np.tolist(self.a),  # converison de matrice à liste
+            "b": np.tolist(self.b),
+            "x": np.tolist(self.x),
+            "time": self.time,
+        }
+        return json.dumps(task_dict)
+
+    @staticmethod
+    def from_json(text: str):
+        task_dict = json.loads(text)
+        task = Task()
+        task.identifier = task_dict["identifier"]
+        task.size = task_dict["size"]
+        task.a = np.array(task_dict["a"])
+        task.b = np.array(task_dict["b"])
+        task.x = np.array(task_dict["x"])
+        task.time = task_dict["time"]
+        return task
+
+    def __eq__(self, other: "Task"):
+        if not isinstance(other, Task):
+            return False
+        return (
+            self.identifier == other.identifier
+            and self.size == other.size
+            and np.array_equal(self.a, other.a)
+            and np.array_equal(self.b, other.b)
+            and np.array_equal(self.x, other.x)
+            and self.time == other.time
+        )

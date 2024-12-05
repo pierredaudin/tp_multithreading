4733c240bf483b728ec709a9f303ec2107de1263
Pierre Daudin <pierre.daudin@univ-tlse3.fr>
2024-12-05 14:38:31 +0100

    ajout conversion json

--- test_task.py
+++ test_task.py
@@ -9,6 +9,12 @@ class TestTask(unittest.TestCase):
         t.work()
         numpy.testing.assert_allclose(t.a @ t.x, t.b)
 
+    def test_serail_deserial():
+        a = Task()
+        txt = a.to_json()
+        b = Task.from_json(txt)
+        numpy.testing.assert_equal(a, b)
+
 
 if __name__ == "__main__":
     unittest.main()
